17f7dc6c8c9b7b8bde09b1b2cc85a4dcd2e2fc22
bingyanglin <bingyang.lin@iota.org>
2021-02-10 15:41:00 +0800

    Use storage operations for pruning

--- bee-tangle/src/pruning/mod.rs
+++ bee-tangle/src/pruning/mod.rs
@@ -3,11 +3,21 @@
 
 mod config;
 
-use crate::MsTangle;
-use bee_message::{milestone::MilestoneIndex, MessageId};
-pub use config::{PruningConfig, PruningConfigBuilder};
+use crate::{
+    MsTangle,
+    {metadata::MessageMetadata, unconfirmed_message::UnconfirmedMessage},
+};
+use bee_message::{
+    milestone::{Milestone, MilestoneIndex},
+    payload::{indexation::HashedIndex, Payload},
+    solid_entry_point::SolidEntryPoint,
+    Message, MessageId,
+};
+use bee_storage::access::{Batch, Delete, Fetch};
 use futures::executor;
 use hashbrown::HashMap;
+
+pub use config::{PruningConfig, PruningConfigBuilder};
 
 #[allow(dead_code)]
 pub(crate) const SOLID_ENTRY_POINT_CHECK_THRESHOLD_PAST: u32 = 50;
@@ -21,9 +31,10 @@ use log::{info, warn};
 #[allow(dead_code)]
 pub enum Error {
     MilestoneNotFoundInTangle(u32),
+    Storage(Box<dyn std::error::Error + Send>),
 }
 
-// // TODO testing
+/// Get the new solid entry points.
 #[allow(dead_code)]
 pub async fn get_new_solid_entry_points<B: StorageBackend>(
     tangle: &MsTangle<B>,
@@ -38,12 +49,16 @@ pub async fn get_new_solid_entry_points<B: StorageBackend>(
                 message_id,
                 |_, _, metadata| async move {
                     // collect all msg that were referenced by that milestone or newer
-                    metadata.milestone_index().is_some() && *metadata.milestone_index().unwrap() >= index
+                    if let Some(milestone_index) = metadata.milestone_index() {
+                        *milestone_index >= index
+                    } else {
+                        false
+                    }
                 },
                 |id, _, metadata| {
                     if !executor::block_on(tangle.is_solid_entry_point(id)) {
-                        if metadata.milestone_index().is_none() {
-                            solid_entry_points.insert(*id, metadata.milestone_index().unwrap());
+                        if let Some(milestone_index) = metadata.milestone_index() {
+                            solid_entry_points.insert(*id, milestone_index);
                         }
                     }
                 },
@@ -58,31 +73,112 @@ pub async fn get_new_solid_entry_points<B: StorageBackend>(
     Ok(solid_entry_points)
 }
 
-// TODO: remove the unconfirmed messages in the database and return (the number of pruned txs,
-//       the number of chcked messages)
+/// Prune the unconfirmed (i.e., unreferenced) messages in the database, and return the number of pruned messages.
 #[allow(dead_code)]
-pub async fn prune_unreferenced_messages(_purning_milestone_index: &MilestoneIndex) -> (usize, usize) {
-    unimplemented!()
+pub async fn prune_unreferenced_messages<B: StorageBackend>(
+    storage: &B,
+    purning_milestone_index: &MilestoneIndex,
+) -> Result<usize, Error> {
+    // Column pairs those should be deleted form the database
+    let mut message_ids: Vec<MessageId> = Vec::new();
+    let mut hasheds_id_to_prune: Vec<(HashedIndex, MessageId)> = Vec::new();
+    let mut messages_id_pairs_to_prune: Vec<(MessageId, MessageId)> = Vec::new();
+
+    // Get the unconfirmed (unreferenced) messages
+    let unconfirmed_messages =
+        Fetch::<MilestoneIndex, Vec<UnconfirmedMessage>>::fetch(&*storage, &purning_milestone_index)
+            .await
+            .map_err(|e| Error::Storage(Box::new(e)))?
+            .unwrap();
+    // Record the count of unconfirmed (unreferenced) messages to prune
+    let pruned_unconfirmed_message_count = unconfirmed_messages.len();
+
+    // Start the batch operation
+    let mut batch = B::batch_begin();
+    for msg in unconfirmed_messages {
+        // Delete the (MilestoneIndex, UnconfirmedMessage) pairs
+        Batch::<(MilestoneIndex, UnconfirmedMessage), ()>::batch_delete(
+            &*storage,
+            &mut batch,
+            &(*purning_milestone_index, msg),
+        )
+        .map_err(|e| Error::Storage(Box::new(e)))?;
+
+        // Record other pairs to be deleted
+        let msg_content = Fetch::<MessageId, Message>::fetch(&*storage, &*msg)
+            .await
+            .map_err(|e| Error::Storage(Box::new(e)))?
+            .unwrap();
+        message_ids.push(*msg.message_id());
+        if let Some(Payload::Indexation(payload)) = msg_content.payload() {
+            hasheds_id_to_prune.push((payload.hash(), *msg));
+        }
+        for parent in msg_content.parents() {
+            messages_id_pairs_to_prune.push((*parent, *msg))
+        }
+    }
+    // Prune the (MilestoneIndex, UnconfirmedMessage) pairs
+    storage
+        .batch_commit(batch, true)
+        .await
+        .map_err(|e| Error::Storage(Box::new(e)))?;
+
+    // Prune the messages
+    prune_messages(storage, message_ids, hasheds_id_to_prune, messages_id_pairs_to_prune).await?;
+    Ok(pruned_unconfirmed_message_count)
 }
 
-// TODO remove the confirmed transactions in the database and return the deleted tx count.
+/// Prunes the (`MessageId`, `Message`), (`MessageId`, `MessageMetadata`), (`HashedIndex`, `MessageId`), and
+/// (`MessageId`, `MessageId`) pairs from the database.
 #[allow(dead_code)]
-pub async fn prune_messages(_hashes: Vec<MessageId>) -> usize {
-    unimplemented!()
+pub async fn prune_messages<B: StorageBackend>(
+    storage: &B,
+    message_ids: Vec<MessageId>,
+    hasheds_id_to_prune: Vec<(HashedIndex, MessageId)>,
+    messages_id_pairs_to_prune: Vec<(MessageId, MessageId)>,
+) -> Result<(), Error> {
+    let mut batch = B::batch_begin();
+    for id in message_ids {
+        Batch::<MessageId, Message>::batch_delete(&*storage, &mut batch, &id)
+            .map_err(|e| Error::Storage(Box::new(e)))?;
+        Batch::<MessageId, MessageMetadata>::batch_delete(&*storage, &mut batch, &id)
+            .map_err(|e| Error::Storage(Box::new(e)))?;
+    }
+    for (hashed_id, id) in hasheds_id_to_prune {
+        Batch::<(HashedIndex, MessageId), ()>::batch_delete(&*storage, &mut batch, &(hashed_id, id))
+            .map_err(|e| Error::Storage(Box::new(e)))?;
+    }
+    for (parent, child) in messages_id_pairs_to_prune {
+        Batch::<(MessageId, MessageId), ()>::batch_delete(&*storage, &mut batch, &(parent, child))
+            .map_err(|e| Error::Storage(Box::new(e)))?;
+    }
+    storage
+        .batch_commit(batch, true)
+        .await
+        .map_err(|e| Error::Storage(Box::new(e)))?;
+    Ok(())
 }
 
-// TODO prunes the milestone metadata and the ledger diffs from the database for the given milestone.
+/// Prunes the `Milestone` and `OutputDiff` from the database.
 #[allow(dead_code)]
-pub async fn prune_milestone(_milestone_index: MilestoneIndex) {
-    // Delete ledger_diff for milestone with milestone_index.
-    // Delete milestone storage (if we have this) for milestone with milestone_index.
-    unimplemented!()
+pub async fn prune_milestone<B: StorageBackend>(storage: &B, milestone_index: MilestoneIndex) -> Result<(), Error> {
+    // Delete milestone.
+    Delete::<MilestoneIndex, Milestone>::delete(&*storage, &milestone_index)
+        .await
+        .map_err(|e| Error::Storage(Box::new(e)))?;
+    // TODO: solve the OutputDiff cyclic dependency issue
+    // Delete output_diff.
+    // Delete::<MilestoneIndex, OutputDiff>::delete(&*storage, &milestone_index)
+    //     .await
+    //     .map_err(|e| Error::Storage(Box::new(e)))?;
+    Ok(())
 }
 
 // NOTE we don't prune cache, but only prune the database.
 #[allow(dead_code)]
 pub async fn prune_database<B: StorageBackend>(
     tangle: &MsTangle<B>,
+    storage: &B,
     mut target_index: MilestoneIndex,
 ) -> Result<(), Error> {
     let target_index_max = MilestoneIndex(
@@ -99,24 +195,27 @@ pub async fn prune_database<B: StorageBackend>(
 
     // TODO update the whole solid_entry_points in the static MsTangle w/o looping.
     for (id, milestone_index) in new_solid_entry_points.into_iter() {
-        tangle.add_solid_entry_point(id, milestone_index).await;
+        tangle
+            .add_solid_entry_point(SolidEntryPoint::from(id), milestone_index)
+            .await;
     }
 
     // We have to set the new solid entry point index.
     // This way we can cleanly prune even if the pruning was aborted last time.
     tangle.update_entry_point_index(target_index);
 
-    prune_unreferenced_messages(&tangle.get_pruning_index()).await;
+    prune_unreferenced_messages(storage, &tangle.get_pruning_index()).await?;
 
     // Iterate through all milestones that have to be pruned.
     for milestone_index in *tangle.get_pruning_index() + 1..*target_index + 1 {
         info!("Pruning milestone {}...", milestone_index);
 
-        let (mut deleted_transactions, mut checked_mesages) =
-            prune_unreferenced_messages(&MilestoneIndex(milestone_index)).await;
+        let mut deleted_messages = prune_unreferenced_messages(storage, &MilestoneIndex(milestone_index)).await?;
 
         if let Some(milestone_message_id) = tangle.get_milestone_message_id(MilestoneIndex(milestone_index)).await {
             let mut messages_to_prune: Vec<MessageId> = Vec::new();
+            let mut hasheds_id_to_prune: Vec<(HashedIndex, MessageId)> = Vec::new();
+            let mut messages_id_pairs_to_prune: Vec<(MessageId, MessageId)> = Vec::new();
 
             // Traverse the approvees of the milestone transaction.
             traversal::visit_parents_depth_first(
@@ -127,23 +226,37 @@ pub async fn prune_database<B: StorageBackend>(
                     //      (even transactions of older milestones)
                     async move { true }
                 },
-                |id, _, _| messages_to_prune.push(*id),
+                |id, msg, _| {
+                    messages_to_prune.push(*id);
+                    if let Some(Payload::Indexation(payload)) = msg.payload() {
+                        hasheds_id_to_prune.push((payload.hash(), *id));
+                    }
+                    for parent in msg.parents() {
+                        messages_id_pairs_to_prune.push((*parent, *id))
+                    }
+                },
                 |_, _, _| {},
                 |_| {},
             )
             .await;
 
-            // // NOTE The metadata of solid entry points can be deleted from the database,
-            // //      because we only need the hashes of them, and don't have to trace their parents.
-            prune_milestone(MilestoneIndex(milestone_index)).await;
+            // NOTE The metadata of solid entry points can be deleted from the database,
+            //      because we only need the hashes of them, and don't have to trace their parents.
+            prune_milestone(storage, MilestoneIndex(milestone_index)).await?;
 
-            checked_mesages += messages_to_prune.len();
-            deleted_transactions += prune_messages(messages_to_prune).await;
+            deleted_messages += messages_to_prune.len();
+            prune_messages(
+                storage,
+                messages_to_prune,
+                hasheds_id_to_prune,
+                messages_id_pairs_to_prune,
+            )
+            .await?;
 
             tangle.update_pruning_index(MilestoneIndex(milestone_index));
             info!(
-                "Pruning milestone {}. Pruned {}/{} messages.",
-                milestone_index, deleted_transactions, checked_mesages
+                "Pruning milestone {}. Pruned {} messages.",
+                milestone_index, deleted_messages
             );
             // TODO trigger pruning milestone index changed event if needed.
             //      notify peers about our new pruning milestone index by
--- bee-tangle/src/storage.rs
+++ bee-tangle/src/storage.rs
@@ -1,20 +1,30 @@
 // Copyright 2020 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::metadata::MessageMetadata;
+use crate::{metadata::MessageMetadata, unconfirmed_message::UnconfirmedMessage};
 
+// use bee_ledger::model::OutputDiff;
 use bee_message::{
     milestone::{Milestone, MilestoneIndex},
+    payload::indexation::HashedIndex,
     Message, MessageId,
 };
 use bee_snapshot::storage::StorageBackend as SnapshotStorageBackend;
 use bee_storage::{
-    access::{Fetch, Insert},
+    access::{Batch, BatchBuilder, Delete, Fetch, Insert},
     backend,
 };
 
 pub trait StorageBackend:
     backend::StorageBackend
+    + BatchBuilder
+    + Batch<MessageId, Message>
+    + Batch<(MessageId, MessageId), ()>
+    + Batch<MessageId, MessageMetadata>
+    + Batch<(MilestoneIndex, UnconfirmedMessage), ()>
+    + Batch<(HashedIndex, MessageId), ()>
+    + Delete<MilestoneIndex, Milestone>
+    // + Delete<MilestoneIndex, OutputDiff>
     + Insert<MessageId, Message>
     + Insert<MessageId, MessageMetadata>
     + Insert<(MessageId, MessageId), ()>
@@ -23,12 +33,21 @@ pub trait StorageBackend:
     + Fetch<MessageId, MessageMetadata>
     + Fetch<MessageId, Vec<MessageId>>
     + Fetch<MilestoneIndex, Milestone>
+    + Fetch<MilestoneIndex, Vec<UnconfirmedMessage>>
     + SnapshotStorageBackend
 {
 }
 
 impl<T> StorageBackend for T where
     T: backend::StorageBackend
+        + BatchBuilder
+        + Batch<MessageId, Message>
+        + Batch<(MessageId, MessageId), ()>
+        + Batch<MessageId, MessageMetadata>
+        + Batch<(MilestoneIndex, UnconfirmedMessage), ()>
+        + Batch<(HashedIndex, MessageId), ()>
+        + Delete<MilestoneIndex, Milestone>
+        // + Delete<MilestoneIndex, OutputDiff>
         + Insert<MessageId, Message>
         + Insert<MessageId, MessageMetadata>
         + Insert<(MessageId, MessageId), ()>
@@ -37,6 +56,7 @@ impl<T> StorageBackend for T where
         + Fetch<MessageId, MessageMetadata>
         + Fetch<MessageId, Vec<MessageId>>
         + Fetch<MilestoneIndex, Milestone>
+        + Fetch<MilestoneIndex, Vec<UnconfirmedMessage>>
         + SnapshotStorageBackend
 {
 }
